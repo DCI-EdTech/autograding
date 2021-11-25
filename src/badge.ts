e1e17c8908340899951d0ea9b807d5bea01e7992
galymax <werkhowski@gmail.com>
2021-11-25 22:01:10 +0100

    generate svg

--- src/badge.ts
+++ src/badge.ts
@@ -1,32 +1,28 @@
-// returns a window with a document and an svg root node
 // @ts-nocheck
-import { createSVGWindow } from 'svgdom'
-import { SVG, registerWindow } from '@svgdotjs/svg.js'
+import svg from 'svg-builder'
 
-export default function createBadge(text) {
-  const window = createSVGWindow()
-  const document = window.document
-  const svg = new SVG(document.body).size(100, 100)
-  const root = svg.group()
-  const textNode = root.text(text).font({
-    family: 'sans-serif',
-    size: '24px',
-    anchor: 'middle',
-    leading: '1em',
-  })
-  const textBBox = textNode.bbox()
-  const textWidth = textBBox.width
-  const textHeight = textBBox.height
-  const padding = 10
-  const width = textWidth + padding * 2
-  const height = textHeight + padding * 2
-  const background = root.rect(width, height).fill('#fff')
-  const foreground = root.rect(width, height).fill('#000')
-  const badge = root.group().add(background).add(foreground).add(textNode)
-  badge.move(0, 0)
-  badge.transform({
-    x: -width / 2,
-    y: -height / 2,
-  })
-  return svg.svg()
+export default function badge(text) {
+  return svg
+    .circle({
+        r: 40,
+        fill: 'none',
+        'stroke-width': 1,
+        stroke: '#CB3728',
+        cx: 42,
+        cy: 82
+    }).circle({
+        r: 40,
+        fill: 'none',
+        'stroke-width': 1,
+        stroke: '#3B92BC',
+        cx: 84,
+        cy: 82
+    }).text({
+        x: 10,
+        y: 20,
+        'font-family': 'helvetica',
+        'font-size': 15,
+        stroke : '#fff',
+        fill: '#fff'
+    }, text).render();
 }
