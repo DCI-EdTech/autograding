90c65eeccc2146af70b21776956fa2a8e2864ffb
galymax <werkhowski@gmail.com>
2021-11-25 22:38:22 +0100

    generate svg

--- dist/index.js
+++ dist/index.js
@@ -12052,8 +12052,8 @@ function badge(text) {
         .width(100)
         .height(100)
         .text({
-        x: 2,
-        y: 1,
+        x: 50,
+        y: 50,
         'font-family': 'helvetica',
         'font-size': 15,
         stroke: '#000',
