caf5f2ab9eaed7a54890b94bdc5df7c37192e35a
galymax <werkhowski@gmail.com>
2021-11-25 23:41:54 +0100

    generate svg

--- src/badge.ts
+++ src/badge.ts
@@ -1,10 +1,15 @@
 // @ts-nocheck
-import SvgText from 'svg-text';
+import svg from 'svg-builder'
 
 export default function badge(text) {
-  return new SvgText({
-    text,
-    maxWidth: 100,
-    textOverflow: 'ellipsis',
-  }).svg
+  return svg
+    .width(200)
+    .height(100)
+    .text({
+        x: 4,
+        y: 25,
+        'font-family': 'helvetica',
+        'font-size': 15,
+        fill: '#000',
+    }, text + ' ✅').render();
 }
