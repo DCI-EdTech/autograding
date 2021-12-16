fb4e17c836d65f27117d5b42d09fba524d88ba2c
galymax <werkhowski@gmail.com>
2021-12-16 12:04:40 +0100

    additional setup

--- dist/index.js
+++ dist/index.js
@@ -6166,6 +6166,8 @@ function default_1(testsDir, packageJsonPath) {
         const pointsPerTask = Math.round(100 / list.length);
         const additionalSetup = packageJson.autograding && packageJson.autograding.setup;
         const testOpts = packageJson.autograding && packageJson.autograding.testOpts;
+        console.log('package.json', JSON.stringify(packageJson));
+        console.log('additionalSetup', additionalSetup);
         console.log('Install', `npm install --ignore-scripts${additionalSetup ? ' && ' + additionalSetup : ''}`);
         return {
             "name": `Task ${item.taskName}`,
