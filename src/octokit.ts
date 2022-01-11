f45488cca7a255afe4cb768148eca2b36a45a7d1
galymax <werkhowski@gmail.com>
2022-01-11 21:22:40 +0100

    commit multiple

--- src/octokit.ts
+++ src/octokit.ts
@@ -27,7 +27,7 @@ function createOctokit() {
     try {
       // create blobs
       const blobs = await Promise.all(files.map(async (file) => {
-        await octokit.git.createBlob({
+        await octokit.rest.git.createBlob({
           owner,
           repo,
           content: file.content,
