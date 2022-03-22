552bbf1947feaf34bd14f19009c17e5bd454fac5
galymax <werkhowski@gmail.com>
2022-03-22 22:24:11 +0100

    link icons

--- src/modifyReadme.ts
+++ src/modifyReadme.ts
@@ -86,9 +86,9 @@ async function addAutogradingInfo(fullReadme, results) {
 
 ${generateResult(results)}
 
-[Results Details](${repoURL}/actions)
+[🔬 Results Details](${repoURL}/actions)
 
-[Give Feedback or Report Problem](https://docs.google.com/forms/d/e/1FAIpQLSfS8wPh6bCMTLF2wmjiE5_UhPiOEnubEwwPLN_M8zTCjx5qbg/viewform?usp=pp_url&entry.2115011968=${encodeURIComponent('https://github.com/')}${encodeURIComponent(process.env.GITHUB_REPOSITORY)})
+[📢 Give Feedback or Report Problem](https://docs.google.com/forms/d/e/1FAIpQLSfS8wPh6bCMTLF2wmjiE5_UhPiOEnubEwwPLN_M8zTCjx5qbg/viewform?usp=pp_url&entry.2115011968=${encodeURIComponent('https://github.com/')}${encodeURIComponent(process.env.GITHUB_REPOSITORY)})
 
 ### Debugging your code
 > [reading the test outputs](https://github.com/DCI-EdTech/autograding-setup/wiki/Reading-test-outputs)
