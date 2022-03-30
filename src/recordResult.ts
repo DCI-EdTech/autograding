a1057dc78a1d3851cf1671fbd608672cbcf3a7ec
galymax <werkhowski@gmail.com>
2022-03-30 16:19:38 +0200

    recording

--- src/recordResult.ts
+++ src/recordResult.ts
@@ -56,8 +56,6 @@ export default async function recordResult(points, result) {
       repo,
     });
 
-    console.log(JSON.stringify(repository, null, 2))
-
     let {data:commits} = await octokit.rest.repos.listCommits({
       owner,
       repo,
