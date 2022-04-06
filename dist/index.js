4fced04252bcaf556fb08301616dee324f52ff90
galymax <werkhowski@gmail.com>
2022-04-06 10:11:55 +0200

    fix filter

--- dist/index.js
+++ dist/index.js
@@ -108,10 +108,11 @@ async function recordResult(points, result) {
             repo: octokit_1.repo,
             sha: branch,
         }));
+        console.log('commits', JSON.stringify(commits, null, 2));
         commits = commits.filter(commit => !commit.author.login.includes('[bot]'));
         // NOTE: doesn't record when students accept but don't submit anything
         // Another solution is needed to prevent recording when teachers create class template from main template
-        if (commits.length < 2 || commits.length && commits[0].author && commits[0].author.login.includes('[bot]') || process.env.IS_ORIGINAL_TEMPLATE_REPO || repository.is_template)
+        if (commits.length < 2 || commits.length && commits[0].author.login.includes('[bot]') || process.env.IS_ORIGINAL_TEMPLATE_REPO || repository.is_template)
             return;
     }
     catch (error) {
