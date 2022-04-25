0ddded07502a221ad895ae24f311f609ee20d8ed
galymax <werkhowski@gmail.com>
2022-04-25 15:25:39 +0200

    record when runtime error

--- src/recordResult.ts
+++ src/recordResult.ts
@@ -81,7 +81,7 @@ export default async function recordResult(points, result) {
     GITHUB_USER_HTML_URL: runInfo && runInfo.actor.html_url, // VARCHAR
     POINTS: points, // TINYINT
     TEST_HAS_RUNTIME_ERRORS: result.numRuntimeErrorTestSuites > 0, // BOOLEAN
-    TEST_RUNTIME_ERRORS: result.runtimeError ? removeTerminalColoring(result.runtimeError.message).replace('●', '').trim() : '', // TEXT
+    TEST_RUNTIME_ERRORS: result.runtimeError ? removeTerminalColoring(result.runtimeError.message).replace('●', '').replace('›', '').trim() : '', // TEXT
     INVOCATION_ID: process.env.INVOCATION_ID, // VARCHAR
     GITHUB_HEAD_BRANCH: runInfo && runInfo.head_branch, // VARCHAR
     GITHUB_HEAD_COMMIT_MESSAGE: runInfo && runInfo.head_commit.message, // VARCHAR
