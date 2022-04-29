40e152946e30a26b785a1835d8d538fe55469acb
galymax <werkhowski@gmail.com>
2022-04-29 14:23:18 +0200

    switch to live results api url

--- dist/index.js
+++ dist/index.js
@@ -172,7 +172,7 @@ async function recordResult(points, result) {
     // send webhook event
     try {
         const req = https_1.default.request({
-            hostname: 'autograding-results.dci.education',
+            hostname: 'https://autograding-results.dci.education',
             port: 443,
             path: '/api/v1/data',
             method: 'POST',
