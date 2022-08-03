27719c92e42867b7b9bbc16943f2a6f3f54a193d
galymax <werkhowski@gmail.com>
2022-08-03 16:13:32 +0200

    access runner files

--- src/getVisualRegressionResult.ts
+++ src/getVisualRegressionResult.ts
@@ -3,7 +3,7 @@ import fs from 'fs'
 
 export default async function getVisualReressionResult() {
     console.log(process.env)
-    fs.readdir(process.env.GITHUB_WORKSPACE, function (err, files) {
+    fs.readdir(process.env.GITHUB_WORKSPACE + '/__tests__/__image_snapshots__', function (err, files) {
         if (err) {
             return console.log('Unable to scan directory: ' + err);
         } 
