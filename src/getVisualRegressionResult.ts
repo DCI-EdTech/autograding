85c1d2cdc836d69cc3c99b348ed2f949340da6c3
galymax <werkhowski@gmail.com>
2022-08-03 18:49:22 +0200

    add todos

--- src/getVisualRegressionResult.ts
+++ src/getVisualRegressionResult.ts
@@ -9,15 +9,23 @@ export default async function getVisualReressionResult() {
     const octokit: github.GitHub = createOctokit()
     if (!octokit) return
 
+    // TODO: cover __tests__ in /src
+
     const dir = path.join(process.env.GITHUB_WORKSPACE, '__tests__', '__image_snapshots__', '__diff_output__')
     const images = []
 
     try {
         const files = fs.readdirSync(dir);
+
+        // TODO: upload all images from __image_snapshots__ recursively
+
         files.forEach(file => {
             const data = fs.readFileSync(path.join(dir, file), 'binary')
             const buffer = Buffer.from(data, 'binary')
             const content = buffer.toString('base64')
+
+            // TODO: better folder name
+
             images.push({path: `.github/visual-regression-diffs/${file}`, content, encoding: 'base64'})
         });
 
